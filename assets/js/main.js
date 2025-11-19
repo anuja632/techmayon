204e8eb86afd06c809980b99bd9c3b2c8a38c561
anuja632 <abianu632.com>
2025-11-19 17:22:01 +0530

    dsds

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+const scrollBtn = document.getElementById("scroll-top");
+
+// Show / Hide on Scroll
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        scrollBtn.classList.add("show");
+    } else {
+        scrollBtn.classList.remove("show");
+    }
+});
+
+// Smooth scroll to top
+scrollBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
 document.addEventListener("DOMContentLoaded", (event) => {
     // preloader
     const preloader = document.getElementById('preloader');
@@ -210,6 +230,7 @@ new Swiper('.ul-services-slider', {
             }
         }
     });
+
 
 
     // TESTIMONIAL SLIDER
